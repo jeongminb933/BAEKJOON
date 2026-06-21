0ac8e1dab3454936bb689a9b85ba3084ed5f2587
Jeongmin Park <65400179+jeongminb933@users.noreply.github.com>
2026-06-21 20:35:43 +0900

    [Passed] 가장 큰 수 고르기 (131ms, 10MB)

---
+++ trail1/가장 큰 수 고르기/picking-biggest-number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++){
+            array[i] = sc.nextInt();
+
+        }
+        int max = array[0];
+        for (int i = 0; i < 10; i++){
+            if (array[i] > max){
+                max = array[i];
+            }
+
+        }
+        System.out.print(max);
+    }
+}
